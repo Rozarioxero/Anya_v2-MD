64bf6121eb31ff3adff971f71a36ffc605e54cb2
Rozarioxero <akuiiiahuuu@gmail.com>
2024-10-22 02:12:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,29 +8,29 @@ module.exports = {
     /**
      * bot details and parameters
      */
-    botname: process.env.BotName || "Qᴜᴇᴇɴ ᴀɴʏᴀ ᴠ2",
-    footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐀𝐧𝐲𝐚 𝐁𝐨𝐭",
-    prefa: process.env.Prefix || "-",
+    botname: process.env.BotName || " ᴀɴsʜɪғ xᴇʀ ᴠ2",
+    footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲  ᴀɴsʜɪғ xᴇʀ 𝐁𝐨𝐭",
+    prefa: process.env.Prefix || ".",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || " ᴀɴsʜɪғ xᴇʀ",
+    ownernumber: process.env.Owner_Number || "916282524515",
+    instagramId: process.env.Insta || "anshif_xer",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
-    packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
-    socialLink: process.env.Web || "https://github.com/PikaBotz",
-    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
+    author: process.env.Author || " ᴀɴsʜɪғ xᴇʀ 🌚🌙",
+    packname: process.env.PackName || " ᴀɴsʜɪғ xᴇʀ 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
+    socialLink: process.env.Web || "https://github.com/Rozarioxero",
+    groupLink: process.env.GcLink || "https://chat.whatsapp.com/GYwATz9YnKy0c9WygfoY4E",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://ja850738:N9PnkwWXAl90bD0N@cluster0.q9ylq.mongodb.net/",
+    sessionId: process.env.SESSION_ID || "YXR_Queen-Anya_3ejR_Queen-Anya_4NmI=",
 
     /**
      * thumbnails and medias
